cc542436ee18a9a3dc8108e974e60afc62d17326
Rowdy Goat <dbrockman28@gmail.com>
2020-10-15 23:29:22 -0500

    added task type

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,16 @@ let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 let createTaskHandler = function(event) {
+    //Keeps page from refreshing with every submission
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
