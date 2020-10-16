728e81d8585cbe3a3a10c5ae950ac4d8c383667e
Rowdy Goat <dbrockman28@gmail.com>
2020-10-15 23:54:06 -0500

    split task form function into two more consise functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
+let placeholderTask = document.querySelector("#placeholder")
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-let createTaskHandler = function(event) {
+let taskFormHandler = function(event) {
     //Keeps page from refreshing with every submission
     event.preventDefault();
+    // Gets the values for the task name and type
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // Create object to hold task info
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // Use object as argument in creatTaskEl
+    createTaskEl(taskDataObj);
+};
+
+let createTaskEl = function(taskDataObj) {
+    // Creates <li> and gives it a class
+    placeholderTask.remove();
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
+    // Same but for a <div>
     let taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // Add to DOM
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    // Add to page
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
-
-formEl.addEventListener("submit", createTaskHandler);
+// Adds task with button click or when enter key is pressed
+formEl.addEventListener("submit", taskFormHandler);
