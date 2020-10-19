c13382bf07bdcdfc6da28bbd1c8c8b61cc644bfc
Rowdy Goat <dbrockman28@gmail.com>
2020-10-19 16:47:27 -0500

    refactor loadTasks for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -229,40 +229,15 @@ let saveTasks = function() {
 
 let loadTasks = function() {
     placeholderTask.remove();
-    // Get task infor from localStorage
-    // Convert task info strings into an array of object
-    // Loop through tasks array and create elements on page
-    let tasks = localStorage.getItem("tasks",);
-    // console.log(tasks);
-    if (!tasks) {
-        let tasks = [];
+    let savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
         return false;
     }
-    tasks = JSON.parse(tasks);
 
-    for (let i = 0; i < tasks.length; i++) {
-        tasks[i].id = taskIdCounter;
-        let listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable", true);
-        let taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-        let taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-        if (tasks[i].status === "to do") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        } else if (tasks[i].status === "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        } else if (tasks[i].status === "complete") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        taskIdCounter++;
+    savedTasks = JSON.parse(savedTasks);
+    for (let i = 0; i < savedTasks.length; i++) {
+        createTaskEl(savedTasks[i]);
     }
 };
 
